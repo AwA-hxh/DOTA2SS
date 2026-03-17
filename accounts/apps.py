b6c663d044f0449de48a6081c6c33497922a8043
Hua Xinhui <lucer2006@163.com>
2026-03-17 13:20:40 +0000

    fail to create admin automatically

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,14 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='testing',
-                email='admin@example.com',
-                password='testing'
-            )
-
